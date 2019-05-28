d381251cbf32699aba74342291fca1b22d162bcb
Iulia Ivana <imi17@imperial.ac.uk>
2019-05-28 18:43:02 +0100

    Added notice component (debugging)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from "./components/header";
+import Notice from "./components/notice";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Notice />
       </div>
     );
   }
