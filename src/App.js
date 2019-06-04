95fdfbe0b4c3af435b20ec81f36609b2f2493642
nbhat1998 <nbhat1998@gmail.com>
2019-06-04 15:17:59 +0100

    Added basic maps API integration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 //import Header from "./components/header";
 import Header from "./components/header";
-
+import GoogleApiWrapper from "./components/mapsAPI";
 class App extends Component {
   render() {
     return <Header />;
---
+++ src/components/mapsAPI.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { GoogleApiWrapper, InfoWindow, Map, Marker } from "google-maps-react";
+class GoogleMapsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+      stores: [
+        { lat: 47.49855629475769, lng: -122.14184416996333 },
+        { latitude: 47.359423, longitude: -122.021071 },
+        { latitude: 47.2052192687988, longitude: -121.988426208496 },
+        { latitude: 47.6307081, longitude: -122.1434325 },
+        { latitude: 47.3084488, longitude: -122.2140121 },
+        { latitude: 47.5524695, longitude: -122.0425407 }
+      ]
+    };
+    // binding this to event-handler functions
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClick = this.onMapClick.bind(this);
+  }
+  displayMarkers = () => {
+    return this.state.stores.map((store, index) => {
+      return (
+        <Marker
+          key={index}
+          id={index}
+          position={{
+            lat: store.latitude,
+            lng: store.longitude
+          }}
+          onClick={() => console.log("You clicked me!")}
+        />
+      );
+    });
+  };
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  };
+  onMapClick = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+  render() {
+    const style = {
+      width: "100vw",
+      height: "100vh",
+      marginLeft: "auto",
+      marginRight: "auto"
+    };
+    return (
+      <Map
+        item
+        xs={12}
+        style={style}
+        google={this.props.google}
+        onClick={this.onMapClick}
+        zoom={14}
+        initialCenter={{ lat: 39.648209, lng: -75.711185 }}
+      >
+        {this.displayMarkers()}
+        {/* <Marker
+          onClick={this.onMarkerClick}
+          title={"Changing Colors Garage"}
+          position={{ lat: 39.648209, lng: -75.711185 }}
+          name={"Changing Colors Garage"}
+        /> */}
+        {/* <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+        /> */}
+      </Map>
+    );
+  }
+}
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyDZ1I2hp5wKo2WoV5XfVPFZf2BJ7QhCpgI"
+})(GoogleMapsContainer);
