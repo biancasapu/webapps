e34166511b3dd68aaa8c78f807242e293134a8e4
nbhat1998 <nbhat1998@gmail.com>
2019-06-04 17:28:16 +0100

    Added a current location pointer to maps, so that map is now instantiated with user's location. remvoed old dummy locations

--- src/components/mapsAPI.jsx
+++ src/components/mapsAPI.jsx
@@ -1,46 +1,56 @@
 import React from "react";
-import { GoogleApiWrapper, Map, Marker } from "google-maps-react";
+import { GoogleApiWrapper, InfoWindow, Map, Marker } from "google-maps-react";
+import CurrentLocation from "./maps";
+
 class GoogleMapsContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       showingInfoWindow: false,
       activeMarker: {},
-      selectedPlace: {},
-      stores: [
-        { lat: 47.49855629475769, lng: -122.14184416996333 },
-        { latitude: 47.359423, longitude: -122.021071 },
-        { latitude: 47.2052192687988, longitude: -121.988426208496 },
-        { latitude: 47.6307081, longitude: -122.1434325 },
-        { latitude: 47.3084488, longitude: -122.2140121 },
-        { latitude: 47.5524695, longitude: -122.0425407 }
-      ]
+      selectedPlace: {}
+      // stores: [
+      //   { lat: 47.49855629475769, lng: -122.14184416996333 },
+      //   { latitude: 47.359423, longitude: -122.021071 },
+      //   { latitude: 47.2052192687988, longitude: -121.988426208496 },
+      //   { latitude: 47.6307081, longitude: -122.1434325 },
+      //   { latitude: 47.3084488, longitude: -122.2140121 },
+      //   { latitude: 47.5524695, longitude: -122.0425407 }
+      // ]
     };
     // binding this to event-handler functions
     this.onMarkerClick = this.onMarkerClick.bind(this);
     this.onMapClick = this.onMapClick.bind(this);
   }
-  displayMarkers = () => {
-    return this.state.stores.map((store, index) => {
-      return (
-        <Marker
-          key={index}
-          id={index}
-          position={{
-            lat: store.latitude,
-            lng: store.longitude
-          }}
-          onClick={() => console.log("You clicked me!")}
-        />
-      );
-    });
-  };
+  // displayMarkers = () => {
+  //   return this.state.stores.map((store, index) => {
+  //     return (
+  //       <Marker
+  //         key={index}
+  //         id={index}
+  //         position={{
+  //           lat: store.latitude,
+  //           lng: store.longitude
+  //         }}
+  //         onClick={() => console.log("You clicked me!")}
+  //       />
+  //     );
+  //   });
+  // };
   onMarkerClick = (props, marker, e) => {
     this.setState({
       selectedPlace: props,
       activeMarker: marker,
       showingInfoWindow: true
     });
+  };
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
   };
   onMapClick = props => {
     if (this.state.showingInfoWindow) {
@@ -67,17 +77,24 @@ class GoogleMapsContainer extends React.Component {
         zoom={14}
         initialCenter={{ lat: 39.648209, lng: -75.711185 }}
       >
-        {this.displayMarkers()}
-        {/* <Marker
+        <CurrentLocation
+          centerAroundCurrentLocation
+          google={this.props.google}
+        />
+        {/* {this.displayMarkers()} */}
+        <Marker
           onClick={this.onMarkerClick}
-          title={"Changing Colors Garage"}
-          position={{ lat: 39.648209, lng: -75.711185 }}
-          name={"Changing Colors Garage"}
-        /> */}
-        {/* <InfoWindow
+          name={"Kenyatta International Convention Centre"}
+        />
+        <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}
-        /> */}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
       </Map>
     );
   }
