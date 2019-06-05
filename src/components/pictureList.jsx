10f4d143e4e3d031fd541a09d72ed9696ecbaf8e
nbhat1998 <nbhat1998@gmail.com>
2019-06-05 20:31:17 +0100

    Added new component picture list to display images from db in the form of a card

---
+++ src/components/pictureList.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { Row, Col } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardHeader,
+  CardSubtitle
+} from "reactstrap";
+
+class PictureList extends Component {
+  constructor(props) {
+    super(props);
+    //console.log(props);
+    this.state = {
+      pic1: props.pic1,
+      pic2: props.pic2,
+      pic3: props.pic3
+    };
+  }
+
+  render() {
+    return (
+      <Row className="mt-3 ml-2 mr-2">
+        <Col sm="4">
+          <CardImg
+            style={{
+              width: "100%",
+              borderRadius: "10%",
+              marginBottom: "20px"
+            }}
+            src={this.state.pic1 ? this.state.pic1 : null}
+          />
+        </Col>
+        <Col sm="4">
+          <CardImg
+            style={{
+              width: "100%",
+              borderRadius: "10%",
+              marginBottom: "20px"
+            }}
+            src={this.state.pic2 ? this.state.pic2 : null}
+          />
+        </Col>
+        <Col sm="4">
+          <CardImg
+            style={{
+              width: "100%",
+              borderRadius: "10%",
+              marginBottom: "20px"
+            }}
+            src={this.state.pic3 ? this.state.pic3 : null}
+          />
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default PictureList;
